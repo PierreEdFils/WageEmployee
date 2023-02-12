776cb2dedcd0058406841e8c713ec452df87207b
Pierre Fils <epierrefils@gmail.com>
2023-02-11 22:25:44 -0500

    Employee getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -1,10 +1,28 @@
 
 public class Employee {
     public int baseSalary;
-    public int hourlyRate;
+    private int hourlyRate;
 
     public  int calculateWage( int extraHours) {
-        return baseSalary + (extraHours*hourlyRate);
+        return baseSalary + (extraHours* getHourlyRate());
+    }
+     public void setBaseSalary(int baseSalary){
+        if (baseSalary <= 0)
+            throw  new IllegalArgumentException("Salary cannot be 0 or less ");
+        this.baseSalary = baseSalary;
     }
 
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw  new IllegalArgumentException("hourly rate  cannot be 0 or negative ");
+        this.hourlyRate = hourlyRate;
+    }
 }
